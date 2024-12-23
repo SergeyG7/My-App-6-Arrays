f033fbd0202f672a7712e1a07cdf105bb93a37e2
Сергей <gvozd0770@gmail.com>
2024-12-23 20:34:20 +0200

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,21 @@
 package app;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, -8, 49, 80, 34, -92, 23, -15, 64, 22, -74, -29, -40, 55, -3, 61, 79, 43, 46, 19};
+        System.out.println("Елемент масиву: " + Arrays.toString(numbers));
+         int sum = 0;
+         int evenCount = 0;
+         int oddCount = 0;
+         int max = numbers[0];
+         int min = numbers[0];
+         boolean foubdNegative = false;
+         int sumAfterNegative = 0;
+         int countAfterNegative = 0;
+    }
+
+
 }
