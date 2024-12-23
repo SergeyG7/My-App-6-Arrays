daad20902be423e0e4ae016bc907a8cc091dc225
Сергей <gvozd0770@gmail.com>
2024-12-23 21:07:24 +0200

    final

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,9 +12,47 @@ public class Main {
          int oddCount = 0;
          int max = numbers[0];
          int min = numbers[0];
-         boolean foubdNegative = false;
+         boolean foundNegative = false;
          int sumAfterNegative = 0;
          int countAfterNegative = 0;
+
+         for (int number : numbers){
+             if (number < 0){
+                 sum += number;
+             }
+             if (!foundNegative) {
+                 foundNegative = true;
+             }
+             if (number % 2 == 0){
+                 evenCount++;
+             } else {
+                 oddCount++;
+             }
+             if (number > max){
+                 max = number;
+             }
+             if (number < min){
+                 min = number;
+             }
+             if (number >= 0){
+                 sumAfterNegative += number;
+                 countAfterNegative++;
+             }
+         }
+
+        double averageAfterNegative = countAfterNegative > 0
+         ? (double) sumAfterNegative / countAfterNegative
+         : 0;
+
+        System.out.println("Сума від'ємних чисел: " + sum + "\nКількість парних чисел: " +
+                evenCount + "\nКількість непарних чисел: " + oddCount + "\nНайменший елемент: " +
+                min + "\nНайбільший елемент: " + max);
+        if (countAfterNegative > 0){
+            System.out.println("Середнє арифметичне чисел після першого від'ємного: " + averageAfterNegative);
+        } else {
+            System.out.println("Чисел після першого від'ємного немає.");
+        }
+
     }
 
 
